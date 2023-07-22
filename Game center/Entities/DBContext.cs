d189e7506a801bf30811a0c9b0b53e2fbc3f5898
Nika <92479422+nikyn4ik@users.noreply.github.com>
2023-07-23 02:32:25 +0300

    Add project files.

---
+++ Game center/Entities/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace GameCenter.Entities
+{
+    public class DBContext : IdentityDbContext<IdentityUser>
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
